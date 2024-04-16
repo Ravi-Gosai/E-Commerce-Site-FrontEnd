15579a3a8750aa21f966d5d4332b8cdcc19fb1bb
Ravi Gosai <rbgosai86@gmail>
2024-04-16 09:54:33 +0530

    navbar done

---
+++ src/Components/Header.js
@@ -0,0 +1,9 @@
+import classes from './Header.module.css'
+const Header = (props)=>{
+
+
+    return <div className={classes.header}>
+        <h2>E-Commerce Website</h2> 
+    </div>
+}
+export default Header;
---
+++ src/Components/ProductItem.js
@@ -0,0 +1,14 @@
+import classes from './ProductItem.module.css'
+const ProductItem = (props)=>{
+    return(
+        <div className={classes.productitem}>
+            <div>{props.product.title}</div>
+            <div>
+                <img className={classes.image} src={props.product.imageUrl} alt="this is"/>
+            </div>
+            <div>{props.product.price} rs <span><button> add to cart</button></span></div>
+            
+        </div>
+    )
+}
+export default ProductItem;
